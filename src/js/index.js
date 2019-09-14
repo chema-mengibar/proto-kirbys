0bed2d319b757ae7acdd58cc0a8d8bb7b126cddf
Unknown <chemamengibar@gmail.com>
2019-09-14 15:06:43 +0200

    ADD: navigation responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,27 @@
 import getBreakpoint  from './breakpoints.js'
 import debounce from 'lodash/debounce';
 
+
+function openNavigation(){
+  document.getElementById("nav").classList.toggle('open');
+  document.getElementById("nav-trigger").classList.toggle('open');
+  console.log('nav change state')
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('document is ready now!');
+
+  document.getElementById("nav-trigger").addEventListener('click', openNavigation, false);
+
 });
 
 window.addEventListener('resize', debounce(() => {
   console.log('breakpoint is: ', getBreakpoint(window.innerWidth));
+
+
+
 }, 250));
+
+
